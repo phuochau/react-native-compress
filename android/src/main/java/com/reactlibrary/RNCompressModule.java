44dcccaa0385c979912fe6055af36136629c6608
Hau Vo <hau.vophuoc@gmail.com>
2017-04-27 13:35:41 +0700

    Fix android

--- android/src/main/java/com/reactlibrary/RNCompressModule.java
+++ android/src/main/java/com/reactlibrary/RNCompressModule.java
@@ -5,6 +5,9 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
 
 public class RNCompressModule extends ReactContextBaseJavaModule {
 
